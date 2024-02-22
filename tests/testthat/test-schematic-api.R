130b4e218d9a4c9953d48dc05e6bb483f3af9254
lakikowolfe <loren.wolfe@sagebase.org>
2024-02-22 11:28:55 -0800

    update schema url

--- tests/testthat/test-schematic-api.R
+++ tests/testthat/test-schematic-api.R
@@ -9,7 +9,7 @@ dataset_id <- "syn51219090"
 access_token <- Sys.getenv("SYNAPSE_PAT")
 base_url <- Sys.getenv("DFA_SCHEMATIC_API_URL")
 testing_manifest_path <- "test_data/synapse_storage_manifest_dataflow.csv"
-schema_url <- "https://raw.githubusercontent.com/Sage-Bionetworks/data_flow/main/inst/data_model/dataflow_component.jsonld"
+schema_url <- "https://raw.githubusercontent.com/Sage-Bionetworks/data_flow_config/5cc4e536d100c6ef9471c2e0e267724068e6d79a/Demo/dataflow_component.jsonld"
 
 # TEST API ##############################################################################
 
